304daa1df4bd56a75af500670bd3a13c951894af
yuanboz <yuanboz@appstate.edu>
2021-06-15 17:33:30 -0400

    add more data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degrees out. It fells like ' + body.current.feelslike + ' degrees.' )
         }
     })
 }
